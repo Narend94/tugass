0377208c31ee632485b4d78f2f72e140f7535180
Dimas Narendra Putra <112672783+Narend94@users.noreply.github.com>
2025-07-06 21:04:22 +0700

    Add files via upload

---
+++ Sistem-Perpustakaan-Sekolah-main/Sistem-Perpustakaan-Sekolah-main/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/de8de52639.js" crossorigin="anonymous"></script>
+    <title>CuyPerpus.com</title>
+    <link rel="icon" href="assets/logoUrl.png" type="image/png">
+  </head>
+  <body>
+    <!--Navbar-->
+   <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary shadow-sm justify-space-between">
+  <div class="container-fluid">
+    <img src="assets/logoNav.png" alt="logo" width="120px">
+    <a href="sign/link_login.html" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <section id="homeSection" class="p-5">
+      <div class="d-flex flex-wrap justify-content-center">
+        <div class="col mt-5">
+         <h2 class="fw-bold text-success"><span class="text-primary">Cuy</span>Perpus</h2>
+          <p class="mb-4">"Temukan Dunia Pengetahuan di Ujung Jari Anda: <br> Perpustakaan Online <span class="fw-bold">CuyPerpus</span> Membawa Anda ke Dunia Buku Digital."</p>
+          <a class="btn btn-primary" href="sign/link_login.html">Get started</a>
+        </div>
+        <div class="col mt-3">
+          <img src="assets/logoDashboard-transformed.jpeg" width="450px">
+        </div>
+      </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
